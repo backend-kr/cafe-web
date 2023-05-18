de8564735f40050cad67e960e6c1fd9af0701ce6
황광회 <jack7141@nate.com>
2023-05-19 00:00:25 +0900

    [master] cafeDetail 수정

--- src/components/CafeDetail.js
+++ src/components/CafeDetail.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Rating } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const CafeDetails = () => {
     const { title } = useParams();
@@ -18,14 +22,41 @@ const CafeDetails = () => {
         return <div>Loading...</div>;
     }
 
+    const rating = 4.6;
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
     return (
         <Box>
+            <Slider {...settings}>
+                {cafeData.thumbnails.map((thumbnail, index) => (
+                    <div key={index}>
+                        <img src={thumbnail.url} alt={`slide-${index}`} width="100%"/>
+                    </div>
+                ))}
+            </Slider>
             <Typography variant="h4">{cafeData.title}</Typography>
-            <Typography variant="h6">Address: {cafeData.address}</Typography>
+            <Box display="flex" alignItems="center">
+                <Rating value={rating} readOnly />
+                <Typography variant="body2" ml={1}>{rating}</Typography>
+                <Typography variant="body2" ml={1}>({/*리뷰 수*/}5,243)</Typography>
+                <Typography variant="body2" ml={1}>숙소답변(3,236)</Typography>
+            </Box>
+            <Box display="flex" alignItems="center">
+                <LocationOnIcon />
+                <Typography variant="body2" ml={1}>{cafeData.address}</Typography>
+            </Box>
             <Typography variant="body1">Street Address: {cafeData.road_address}</Typography>
             <Typography variant="body1">Phone number: {cafeData.tel}</Typography>
             <Typography variant="body1">Home page: {cafeData.home_page}</Typography>
             <Typography variant="body1">Hours: </Typography>
+
         </Box>
     );
 };
